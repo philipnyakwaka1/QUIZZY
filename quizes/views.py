f5081d1e58603378370ec737df66d34bfe7d4bd6
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-05-27 21:00:17 +0300

    feat: designed a user-friendly register page with fields for username, email, password, and password confirmation

--- quizes/views.py
+++ quizes/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 def home_page(request):
     return render(request, 'quizes/home.html')
 
 def login_page(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'Login successful, you can take your quiz!')
+            return redirect('home-view')
+        else:
+            messages.error(request, 'Invalid username or password')
     return render(request, 'quizes/login.html')
 
 def logout_page(request): 
